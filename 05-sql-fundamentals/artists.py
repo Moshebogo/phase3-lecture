dbbfd4660a62c9a5282be33be01d1af4e1d4c292
Eli Moshe <moshebogo@gmail.com>
2023-02-25 20:03:32 -0500

    added delete def

--- 05-sql-fundamentals/artists.py
+++ 05-sql-fundamentals/artists.py
@@ -3,27 +3,45 @@ import sqlite3
 connection = sqlite3.connect('chinook.db')
 cursor = connection.cursor()
 
-class Artist():
-    def __init__(self, artistId, name):
-        self.artistId = artistId
-        self.name = name
+class Artist:
+    def __init__(self):
+        self.artistId = None
+        self.name = None
 
-def add_artist(self, name):
-    sql = f"""
-    INSERT INTO artists
-    VALUE {(name)}
-    """
-    cursor.execute(sql)
-    connection.commit()
+    def get_artists(self, id1=0, id2=1000):
+        sql = f"""
+        SELECT * 
+        FROM artists
+        WHERE ArtistId  BETWEEN  {id1} AND {id2}
+        """
+        result = cursor.execute(sql).fetchall()
+        return result
+    
+    def add_artist(self, name):
+        sql = f"""
+        INSERT INTO artists (Name)
+        VALUES ('{name}')
+        """
+        cursor.execute(sql)
+        connection.commit()
 
-def get_all_artists(self):
-    sql = f"""
-    SELECT * 
-    FROM artists
-    """
-    cursor.execute(sql)
-    connection.commit()
+    def edit_artist(self):
+        sql = f"""
+        EDIT ***
+        """    
+        cursor.execute(sql)
+        connection.commit()
 
+
+    def delete_artist(self, name):
+        sql = f"""
+        DELETE FROM artists 
+        WHERE Name LIKE ('{name}')
+        """   
+        cursor.execute(sql)
+        connection.commit()
 
 if __name__ == '__main__':
-    pass
+    print(Artist().get_artists())
+    print(Artist().add_artist("Benny Friedman"))
+    print(Artist().delete_artist("Benny Friedman"))
